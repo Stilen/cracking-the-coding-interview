3380b69710c31f47b4cc284c673b991bf49d06af
peman <pemanuelsilva@hotmail.com>
2017-04-14 11:11:21 +0100

    first commit

--- src/tests/TestChapter1.java
+++ src/tests/TestChapter1.java
@@ -1,4 +1,4 @@
-package chapter1;
+package tests;
 
 
 import static org.junit.Assert.*;
@@ -8,6 +8,8 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import chapter1.*;
 
 public class TestChapter1 {
 
